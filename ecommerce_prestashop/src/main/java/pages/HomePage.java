50f9e8bc0b33230e61486076cd6a5276abf9dcbe
Ricardo Varela <ricardomvvarela@gmail.com>
2021-12-09 09:21:13 -0300

    Update HomePage.java

--- ecommerce_prestashop/src/main/java/pages/HomePage.java
+++ ecommerce_prestashop/src/main/java/pages/HomePage.java
@@ -13,7 +13,17 @@ public class HomePage {
 	
 	List<WebElement> listaProdutos = new ArrayList();
 	
+	private By textoProdutosNoCarrinho = By.className("cart-products-count");
+	
 	private By produtos = By.className("product-description");
+	
+	private By descricoesDosProdutos = By.cssSelector(".product-description a");
+	
+	private By precoDosProdutos = By.className("price");
+	
+	private By botaoSignIn = By.cssSelector("#_desktop_user_info span.hidden-sm-down");
+	
+	private By usuarioLogado = By.cssSelector("#_desktop_user_info span.hidden-sm-down");
 	
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
@@ -26,6 +36,40 @@ public class HomePage {
 	
 	private void carregarListaProdutos() { 
 		listaProdutos = driver.findElements(produtos);
+	}
+	
+	public int obterQuantidadeProdutosNoCarrinho() {
+		
+		String quantidadeProdutosNoCarrinho = driver.findElement(textoProdutosNoCarrinho).getText();
+				
+		quantidadeProdutosNoCarrinho = quantidadeProdutosNoCarrinho.replace("(", "");
+		quantidadeProdutosNoCarrinho = quantidadeProdutosNoCarrinho.replace(")", "");
+		
+		int qtdProdutosNoCarrinho = Integer.parseInt(quantidadeProdutosNoCarrinho);
+		
+		return qtdProdutosNoCarrinho;
+	}
+	
+	public String obterNomeProduto(int indice) {
+		return driver.findElements(descricoesDosProdutos).get(indice).getText();
+	}
+	
+	public String obterPrecoProduto (int indice) {
+		return driver.findElements(precoDosProdutos).get(indice).getText();
+	}
+	
+	public ProdutoPage clicarProduto(int indice) {
+		driver.findElements(descricoesDosProdutos).get(indice).click();
+		return new ProdutoPage(driver);
+	}
+	
+	public LoginPage clicarBotaoSignIn() {
+		driver.findElement(botaoSignIn).click();
+		return new LoginPage(driver);
+	}
+	
+	public boolean estaLogado(String texto) {
+		return texto.contentEquals(driver.findElement(usuarioLogado).getText());
 	}
 	
 }
